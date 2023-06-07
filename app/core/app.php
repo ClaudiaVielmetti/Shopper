627a4013eb2a9c8c41669348c97a59c0f7c2b869
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-07 15:08:32 +0200

    comments

--- app/core/app.php
+++ app/core/app.php
@@ -1,23 +1,24 @@
 <?php
-
-Class App 
+// Define a class named App
+class App
 {
 
-    protected $controller = "home";
-    protected $method = "index";
+    protected $controller = "home";    // Define a protected variable named "controller"
+    protected $method = "index";    // Define a protected variable named "method"
 
-
-    public function__construct()
+    public function __construct()    // Constructor method definition
     {
 
-
+        $url = $this->parseURL();    // assign the value of parseURL() function to url
+        print_r($url);    // print the value of url
 
     }
 
-    private function parseURL()
+    private function parseURL()    // Define a private function parseURL()
+
     {
 
-        
+        $url = isset($_GET['url']) ? $_GET['url'] : 'home';    // Check if url is set or not, if not then assign the value of url as "home"
+        return $url;    // Return the value of url
     }
-
 }
