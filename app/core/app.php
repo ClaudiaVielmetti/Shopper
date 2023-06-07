f445dc371f28692929f9f819e8a4179cf3f9c09b
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-07 15:00:07 +0200

    initialise app

--- app/core/app.php
+++ app/core/app.php
@@ -1,2 +1,23 @@
 <?php
 
+Class App 
+{
+
+    protected $controller = "home";
+    protected $method = "index";
+
+
+    public function__construct()
+    {
+
+
+
+    }
+
+    private function parseURL()
+    {
+
+        
+    }
+
+}
