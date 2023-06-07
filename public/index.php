491194e3b7a8f304967bbc3e53b8da4de2b8fbad
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-07 16:36:15 +0200

    server paths

--- public/index.php
+++ public/index.php
@@ -1,6 +1,18 @@
 <?php
-session_start();
-include "../app/init.php";
+session_start(); //start a session
+
+
+
+
+$path = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF']; //Define server path
+$path = str_replace("index.php", "", $path); //Replace index.php with blank string
+
+
+define('ROOT', $path); //Define Root constant
+define('ASSETS', $path . "assets/");  // Define Assets constant
+include "../app/init.php"; //Include ini.php file
+
+
 
 //initializing the app
-$app = new App();
+$app = new App(); 
