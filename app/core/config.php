12e104583c791452fe129465ad660d53136c7648
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-07 16:47:50 +0200

    Create config.php

---
+++ app/core/config.php
@@ -0,0 +1,20 @@
+<?php
+
+define("WEBSITE_TITLE", "REVERSE");
+
+
+// Database
+define('DB_NAME', "shop_db");
+define('DB_USER', "root");
+define('DB_PASS', "");
+
+
+//when in debug mode = true if not false
+define('DEBUG', true);
+
+if(DEBUG){
+    ini_set('display_errors', 1);
+    
+}else {
+    ini_set('display_errors', 0);
+}
