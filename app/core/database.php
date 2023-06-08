a08a3b4192d799915e1194ef7d97baf8d926d744
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-08 13:10:03 +0200

    Update database.php

--- app/core/database.php
+++ app/core/database.php
@@ -1,19 +1,40 @@
-<?php 
+<?php
 
-Class Database
-{
+class Database
+{    ///
+    // Constructs a new instance of the class, initializing a connection to the
+    // MySQL database.
+    
+    // @throws PDOException if the database connection cannot be established
+    
     public static $con;
     public function __construct()
     {
-        if(self::$con){
+        try {
+            $string = "mysql:host=localhost;dbname=shop_db;charset=utf8";
+            self::$con = new PDO($string, "root", "");
 
+        } catch (PDOException $e) {
+            die($e->getMessage());
         }
     }
-
+    
+     //Retrieves the instance of the class if it exists, otherwise creates it.
+     
+     // @throws Some_Exception_Class Exception thrown if the instance cannot be created
+     // @return self Returns the instance of the class
+     
     public static function getInstance()
     {
-        
+        if (self::$con) {
+
+            return self::$con;
+        }
+
+        $a = new self();
+        return self::$con;
     }
+
 }
 
-$db = new Database();
+$db = Database::getInstance();
