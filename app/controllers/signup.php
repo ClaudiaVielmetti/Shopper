142dfd0484dbb848761f032d71a2006911f9bb8d
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-08 12:18:06 +0200

    db

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -4,6 +4,12 @@ class Signup extends Controller
     public function index()
     {
         $data['page_title'] = "Home"; // set the page title
+
+        if($_SERVER['REQUEST_METHOD'] == "POST")
+        {
+            show($_POST);
+        }
+
         $this->view("signup", $data); // call the view method
 
 
--- app/views/shop/signup.php
+++ app/views/shop/signup.php
@@ -1,22 +1,24 @@
 <?php $this->view("header", $data); ?>
 
-<section id="form"><!--form-->
+
+<section id="form" style="margin-top: 5px;"><!--form-->
     <div class="container">
+        <div class="row" style="text-align: center">
 
-
-        <div class="col-sm-4">
-            <div class="signup-form"><!--sign up form-->
-                <h2>New User Signup!</h2>
-                <form action="#">
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Password" />
-                    <button type="submit" class="btn btn-default">Signup</button>
-                </form>
-            </div><!--/sign up form-->
+            <div class="col-sm-4" style="float: none; display: inline-block">
+                <div class="signup-form"><!--sign up form-->
+                    <h2>New User Signup!</h2>
+                    <form method="POST">
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email Address" />
+                        <input type="password" placeholder="Password" />
+                        <button type="submit" class="btn btn-default">Signup</button>
+                    </form>
+                </div><!--/sign up form-->
+            </div>
         </div>
     </div>
-
 </section><!--/form-->
+
 
 <?php $this->view("footer", $data); ?>
