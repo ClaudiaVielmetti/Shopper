17a5bc32a1fbf56a0af96fe5124b5760777ebf2b
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-08 11:57:56 +0200

    Update login.php

--- app/views/shop/login.php
+++ app/views/shop/login.php
@@ -1,29 +1,30 @@
 <?php $this->view("header", $data); ?>
 
-<section id="form"><!--form-->
+<section id="form" style="margin-top: 5px;"><!--form-->
 	<div class="container">
-		<div class="row">
-			<div class="col-sm-4 col-sm-offset-1">
+		<div class="row" style="text-align: center">
+			<div class="col-sm-4 col-sm-offset-1" style="float: none; display: inline-block">
 				<div class="login-form"><!--login form-->
 					<h2>Login to your account</h2>
 					<form action="#">
 						<input type="text" placeholder="Name" />
 						<input type="email" placeholder="Email Address" />
-						<span>
+						<span style="text-align: center;">
 							<input type="checkbox" class="checkbox">
 							Keep me signed in
 						</span>
+
 						<button type="submit" class="btn btn-default">Login</button>
 					</form>
 					<br>
-					<a href="<?php echo ROOT; ?>signup" >Sign-up Here!</a>
+					<a href="<?php echo ROOT; ?>signup">Sign-up Here!</a>
 				</div><!--/login form-->
 			</div>
 
 
 		</div>
 	</div>
-	
+
 </section><!--/form-->
 
 <?php $this->view("footer", $data); ?>
