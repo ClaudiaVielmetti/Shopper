be46a99f366fcc2ea29f632d0934895151915423
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-08 11:35:50 +0200

    sign up and login

--- app/views/shop/login.php
+++ app/views/shop/login.php
@@ -15,24 +15,15 @@
 						</span>
 						<button type="submit" class="btn btn-default">Login</button>
 					</form>
+					<br>
+					<a href="signup" >Sign-up Here!</a>
 				</div><!--/login form-->
 			</div>
-			<div class="col-sm-1">
-				<h2 class="or">OR</h2>
-			</div>
-			<div class="col-sm-4">
-				<div class="signup-form"><!--sign up form-->
-					<h2>New User Signup!</h2>
-					<form action="#">
-						<input type="text" placeholder="Name" />
-						<input type="email" placeholder="Email Address" />
-						<input type="password" placeholder="Password" />
-						<button type="submit" class="btn btn-default">Signup</button>
-					</form>
-				</div><!--/sign up form-->
-			</div>
+
+
 		</div>
 	</div>
+	
 </section><!--/form-->
 
 <?php $this->view("footer", $data); ?>
