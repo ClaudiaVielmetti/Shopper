5c07674b67f8b518770f0d87ceb7da2c6ff195b2
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-08 15:11:54 +0200

    Update user.class.php

--- app/models/user.class.php
+++ app/models/user.class.php
@@ -10,7 +10,7 @@ class User
         $data['password'] = trim($POST['password']);
         $password2 = trim($POST['password2']);
 
-        if (empty($data['email']) || !preg_match("/^[^0-9][_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})+$/", $data['email'])); {
+        if (empty($data['email']) || !preg_match("/[a-zA-Z_-]+@[a-zA-Z_-]+\.[a-zA-Z_-]+$/", $data['email'])); {
             $this->error .= "Please enter a valid email <br>";
         }
         if (empty($data['name']) || !preg_match("/[a-zA-Z]+$/", $data['name'])); {
@@ -30,7 +30,7 @@ class User
             $data['date'] = date("Y-m-d H:i:s");
 
             $query = "insert into users (url_address, name, email, password, rank, date) values (:url_address, :name, :email, password, :rank, :date)";
-            
+
             $db = Database::getInstance();
             $result = $db->write($query, $data);
 
@@ -52,7 +52,7 @@ class User
 
     private function get_random_string_max($length)
     {
-        $array = array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','x');
+        $array = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'x');
         $text = "";
 
         $length = rand(4, $length);
