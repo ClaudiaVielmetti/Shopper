5ff45a1b8389b6103c48fb2812bbda1a5b20befa
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-12 14:15:05 +0200

    Update signup.php

--- app/views/shop/signup.php
+++ app/views/shop/signup.php
@@ -5,16 +5,16 @@
     <div class="container">
         <div class="row" style="text-align: center">
 
-            
+            <span style="font-size:18px; color: red;"><?php check_error() ?></span>
 
             <div class="col-sm-4" style="float: none; display: inline-block">
                 <div class="signup-form"><!--sign up form-->
                     <h2>New User Signup!</h2>
                     <form method="POST">
-                        <input  name="name"  type="text" placeholder="Name" />
-                        <input  name="email" type="email" placeholder="Email Address" />
-                        <input  name="password" type="password" placeholder="Password" />
-                        <input  name="password2" type="password" placeholder="Re-type Password" />
+                        <input name="name" value="<?= isset($_POST['name']) ? $_POST['name'] : ''; ?>" type="text" placeholder="Name" />
+                        <input name="email" value="<?= isset($_POST['email']) ? $_POST['email'] : ''; ?>" type="email" placeholder="Email Address" />
+                        <input name="password" type="password" placeholder="Password" />
+                        <input name="password2" type="password" placeholder="Retype Password" />
                         <button type="submit" class="btn btn-default">Signup</button>
                     </form>
                 </div><!--/sign up form-->
