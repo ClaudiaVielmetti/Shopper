995c4e2820193ce63f40c1f0e0e69a983b8061e3
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-12 14:20:35 +0200

    Update login.php

--- app/views/shop/login.php
+++ app/views/shop/login.php
@@ -3,17 +3,20 @@
 <section id="form" style="margin-top: 5px;"><!--form-->
 	<div class="container">
 		<div class="row" style="text-align: center">
+
+			<span style="font-size:18px;color:red;"><?php check_error() ?></span>
+
 			<div class="col-sm-4 col-sm-offset-1" style="float: none; display: inline-block">
 				<div class="login-form"><!--login form-->
 					<h2>Login to your account</h2>
 					<form method="post">
-						<input type="text" name="email" placeholder="Email" />
-						<input type="password" name="password" placeholder="Password" />
-						<span style="text-align: center;">
+						<input type="email" name="email" value="<?= isset($_POST['email']) ? $_POST['email'] : ''; ?>" placeholder="Email Address" />
+						<input type="password" name="password" value="<?= isset($_POST['password']) ? $_POST['password'] : ''; ?>" placeholder="Password" />
+						<span>
 							<input type="checkbox" class="checkbox">
 							Keep me signed in
-						</span>
 
+						</span>
 						<button type="submit" class="btn btn-default">Login</button>
 					</form>
 					<br>
