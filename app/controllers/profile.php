da1e56f14808948c9561ab53d6e4ab25089b35c7
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-13 15:15:47 +0200

    profile page

---
+++ app/controllers/profile.php
@@ -0,0 +1,20 @@
+<?php
+class Profile extends Controller
+{
+    public function index()
+    {
+        $user = $this->load_model('User');
+        $user_data = $user->check_login();
+
+        if(is_object($user_data))
+        {
+            $data['user_data'] = $user_data;
+        }
+
+
+        $data['page_title'] = "Profile"; // set the page title
+        $this->view("profile", $data); // call the view method
+
+
+    }
+}
