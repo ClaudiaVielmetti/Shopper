b5a227ae655b1e1d623334914cc619546922f532
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-13 15:20:02 +0200

    profile

--- app/views/shop/admin/index.php
+++ app/views/shop/admin/index.php
@@ -378,29 +378,7 @@
                       		</div>
                       	</div><!-- /col-md-4 -->
                       	
-						<div class="col-md-4 mb">
-							<!-- WHITE PANEL - TOP USER -->
-							<div class="white-panel pn">
-								<div class="white-header">
-									<h5>TOP USER</h5>
-								</div>
-								<p><img src="<?=ASSETS . THEME ?>admin/img/ui-zac.jpg" class="img-circle" width="80"></p>
-								<p><b>Zac Snider</b></p>
-								<div class="row">
-									<div class="col-md-6">
-										<p class="small mt">MEMBER SINCE</p>
-										<p>2012</p>
-									</div>
-									<div class="col-md-6">
-										<p class="small mt">TOTAL SPEND</p>
-										<p>$ 47,60</p>
-									</div>
-								</div>
-							</div>
-						</div><!-- /col-md-4 -->
-                      	
-
-                    </div><!-- /row -->
+				
                     
                     				
 					<div class="row">
@@ -433,7 +411,7 @@
 										<h5>60% Used</h5>
 									</div>
 								</footer>
-                      		</div><! -- /darkblue panel -->
+                      		</div><
 						</div><!-- /col-md-4 -->
 						
 						
