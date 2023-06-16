3d9d79b7568cc87541e7f2043d45e2b8f3313685
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-16 16:35:31 +0200

    ajax

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -35,7 +35,7 @@ class Admin extends Controller
 		$category = $this->load_model("Category");
 		$tbl_rows = $category->make_table($categories_all);
 		$data['tbl_rows'] = $tbl_rows;
-		 $data['categories'] = $categories;
+		$data['categories'] = $categories;
 	 
 		$data['page_title'] = "Admin - Categories";
 		$data['current_page'] = "categories";
