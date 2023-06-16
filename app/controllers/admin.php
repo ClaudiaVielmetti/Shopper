ae1ed51ee88dedb6a07d31379bdb5b419b077b39
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-16 15:37:17 +0200

    Update admin.php

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -17,4 +17,30 @@ class Admin extends Controller
 
 
     }
+
+
+    public function categories()
+	{
+		
+		$User = $this->load_model('User');
+		$user_data = $User->check_login(true, ["admin"]);
+		if(is_object($user_data)){
+			$data['user_data'] = $user_data;
+		}
+
+		$DB = Database::newInstance();
+		$categories_all = $DB->read("select * from categories order by id desc");
+ 		$categories = $DB->read("select * from categories where disabled = 0 order by id desc");
+
+		$category = $this->load_model("Category");
+		$tbl_rows = $category->make_table($categories_all);
+		$data['tbl_rows'] = $tbl_rows;
+		$data['categories'] = $categories;
+	 
+		$data['page_title'] = "Admin - Categories";
+		$data['current_page'] = "categories";
+		$this->view("admin/categories",$data);
+	}
 }
+
+
