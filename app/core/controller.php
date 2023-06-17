a07e85a7075de5785a56dbeefbc19f0abbfcaacc
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-17 12:42:37 +0200

    Update controller.php

--- app/core/controller.php
+++ app/core/controller.php
@@ -1,27 +1,32 @@
-<?php
+<?php 
 
-class Controller
+Class Controller
 {
 
-  public function view($path, $data = [])
-  {
+	public function view($path,$data = [])
+	{
 
-    if (file_exists("../app/views/" . THEME . $path . ".php")) {
-      include "../app/views/" .  THEME . $path . ".php";
-    } else {
-      include "../app/views/" .  THEME . "404.php";
-    }
-  }
+		if(is_array($data)){
+ 			extract($data);
+		}
 
+		if(file_exists("../app/views/" . THEME . $path . ".php"))
+		{
+			include "../app/views/" . THEME . $path . ".php";
+		}else{
+			include "../app/views/" . THEME . "404.php";
+		}
+	}
 
-  public function load_model($model)
-  {
+	public function load_model($model)
+	{
 
-    if (file_exists("../app/models/" . strtolower($model) . ".class.php")) {
-      include "../app/models/" . strtolower($model). ".class.php";
-      return $a = new $model();
-    } 
+		if(file_exists("../app/models/" . strtolower($model) . ".class.php"))
+		{
+			include_once "../app/models/" . strtolower($model) . ".class.php";
+			return $a = new $model();
+		}
 
-    return false;
-  }
+		return false;
+	}
 }
