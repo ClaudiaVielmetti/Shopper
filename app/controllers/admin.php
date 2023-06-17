344583ffc4c1fd547392a3e381c6f45ce0885fd9
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-17 13:13:31 +0200

    Update admin.php

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -41,6 +41,32 @@ class Admin extends Controller
 		$data['current_page'] = "categories";
 		$this->view("admin/categories",$data);
 	}
+
+	public function products()
+	{
+		
+		$User = $this->load_model('User');
+		$user_data = $User->check_login(true, ["admin"]);
+		if(is_object($user_data)){
+			$data['user_data'] = $user_data;
+		}
+
+		$DB = Database::newInstance();
+		$products = $DB->read("select * from products order by id desc");
+
+ 		$categories = $DB->read("select * from categories where disabled = 0 order by id desc");
+
+		$product = $this->load_model("Product");
+		$category = $this->load_model("Category");
+		
+		$tbl_rows = $product->make_table($products,$category);
+		$data['tbl_rows'] = $tbl_rows;
+		$data['categories'] = $categories;
+	 
+		$data['page_title'] = "Admin - Products";
+		$data['current_page'] = "products";
+		$this->view("admin/products",$data);
+	}
 }
 
 
