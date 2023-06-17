c9fbf030839e79e7333ae50a0ad2f3ed2aa014cc
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-17 15:05:36 +0200

    Update app.php

--- app/core/app.php
+++ app/core/app.php
@@ -1,51 +1,43 @@
 <?php
-// Define a class named App
+
 class App
 {
 
-    protected $controller = "home";    // Define a protected variable named "controller"
-    protected $method = "index";    // Define a protected variable named "method"
-    protected $params = [];    // Define a protected variable named "params"
+    protected $controller = "home";
+    protected $method = "index";
+    protected $params;
 
-    public function __construct()    // Constructor method definition
+    public function __construct()
     {
 
-        $url = $this->parseURL();    // assign the value of parseURL() function to url
-  
+        $url = $this->parseURL();
+        $url[0] = str_replace("-", "_", $url[0]);
 
-        if(file_exists("../app/controllers/" . strtolower( $url[0]) . ".php"))    // Check if the file exists in the controllers folder
-        {
-            $this->controller = strtolower($url[0]);    // assign the value of parseURL() function to controller
-            unset($url[0]);    // unset the value of url
+        if (file_exists("../app/controllers/" . strtolower($url[0]) . ".php")) {
 
+            $this->controller = strtolower($url[0]);
+            unset($url[0]);
         }
 
-        require "../app/controllers/" . $this->controller . ".php";    // require the controller
-        $this->controller = new $this->controller;    // assign the value of parseURL() function to controller
+        require "../app/controllers/" . $this->controller . ".php";
+        $this->controller = new $this->controller;
 
-
-        if(isset($url[1]))
-        {
-            $url[1] = strtolower($url[1]);    // assign the value of parseURL() function to method
-            if(method_exists($this->controller, $url[1]))    // Check if the method exists in the controller
-            {
-                
-                $this->method = $url[1];    // assign the value of parseURL() function to method
-                unset($url[1]);    // unset the value of url
+        if (isset($url[1])) {
+            $url[1] = strtolower($url[1]);
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
             }
         }
 
-        $this->params = (count($url) > 0) ? $url : ["home"];    // assign the value of parseURL() function to params
+        $this->params = (count($url) > 0) ? $url : ["home"];
 
-        call_user_func_array([$this->controller, $this->method], $this->params);    // call the method in the controller
-       
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
-    private function parseURL()    // Define a private function parseURL()
-
+    private function parseURL()
     {
-
-        $url = isset($_GET['url']) ? $_GET['url'] : 'home';    // Check if url is set or not, if not then assign the value of url as "home"
-        return explode("/", filter_var(trim($url, "/"), FILTER_SANITIZE_URL));    // Split the url into array and filter the url(Sanitize)
+        $url = isset($_GET['url']) ? $_GET['url'] : "home";
+        return explode("/", filter_var(trim($url, "/"), FILTER_SANITIZE_URL));
     }
 }
