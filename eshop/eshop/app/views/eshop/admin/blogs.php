df9fb137099d86415019b919abf968359dfe8821
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-18 17:31:34 +0200

    up

---
+++ eshop/eshop/app/views/eshop/admin/blogs.php
@@ -0,0 +1,8 @@
+<?php $this->view("admin/header",$data); ?>
+
+<?php $this->view("admin/sidebar",$data); ?>
+
+
+
+
+<?php $this->view("admin/footer",$data); ?>
---
+++ eshop/eshop/app/views/look-book.php
@@ -0,0 +1,6 @@
+<?php $this->view("header",$data); ?>
+	
+
+	
+
+<?php $this->view("footer",$data); ?>
