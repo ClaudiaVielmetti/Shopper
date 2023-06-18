adfe4d02bd5d63ecc3e6c1c9b802c7ed35c7c4e7
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-18 16:29:14 +0200

    Comments functions

--- eshop/eshop/app/core/functions.php
+++ eshop/eshop/app/core/functions.php
@@ -1,3 +1,13 @@
+<!-- /*
+This code prints the value of $data using the "pre" tag.
+The code starts by defining a function named "show" that accepts 1 parameter.
+The code then prints "pre" tags and the value of the parameter.
+The code then prints "pre" tags again.
+The code has another function that returns the value of the parameter after adding backslashes to it.
+The code has another function that redirects the user to the specified link in the website.
+*/ -->
+
+
 <?php 
 
 
