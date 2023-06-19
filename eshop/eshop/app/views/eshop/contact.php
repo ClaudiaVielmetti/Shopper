9be49a995f002435c8a1a455672b7429a08ef3f4
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-06-19 13:11:14 +0200

    map

--- eshop/eshop/app/views/eshop/contact.php
+++ eshop/eshop/app/views/eshop/contact.php
@@ -1,72 +1,75 @@
-<?php $this->view("header",$data); ?>
+<?php $this->view("header", $data); ?>
 
-	 <div id="contact-page" class="container">
-    	<div class="bg">
-	    	<div class="row">    		
-	    		<div class="col-sm-12">    			   			
-					<h2 class="title text-center">Contact <strong>Us</strong></h2>    			    				    				
-					
-				</div>			 		
-			</div>    	
-    		<div class="row">  	
-	    		<div class="col-sm-8">
-	    			<div class="contact-form">
-	    				<h2 class="title text-center">Get In Touch</h2>
+<div id="contact-page" class="container">
+	<div class="bg">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2 class="title text-center">Contact <strong>Us</strong></h2>
 
-	    				<?php if(is_array($errors) && count($errors)>0):?>
-	    					<?php foreach ($errors as $error):?>
-	    						<div class="status alert alert-danger" style=""><?=$error?></div>
-	    					<?php endforeach;?>
-	    				<?php endif;?>
-	    				<?php if(isset($_GET['success'])):?>
-	    					<div class="status alert alert-success" >Message sent successfully</div>
-	    				<?php endif;?>
-				    	<form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
-				            <div class="form-group col-md-6">
-				                <input type="text" name="name" class="form-control" required="required" placeholder="Name" value="<?=isset($POST['name'])? $POST['name']:''?>">
-				            </div>
-				            <div class="form-group col-md-6">
-				                <input type="email" name="email" class="form-control" required="required" placeholder="Email" value="<?=isset($POST['email'])? $POST['email']:''?>">
-				            </div>
-				            <div class="form-group col-md-12">
-				                <input type="text" name="subject" class="form-control" required="required" placeholder="Subject" value="<?=isset($POST['subject'])? $POST['subject']:''?>">
-				            </div>
-				            <div class="form-group col-md-12">
-				                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"><?=isset($POST['subject'])? $POST['subject']:''?></textarea>
-				            </div>                        
-				            <div class="form-group col-md-12">
-				                <input type="submit" class="btn btn-primary pull-right" value="Submit">
-				            </div>
-				        </form>
-	    			</div>
-	    		</div>
-	    		<div class="col-sm-4">
-	    			<div class="contact-info">
-	    				<h2 class="title text-center">Contact Info</h2>
-	    				<address>
-	    					<?=nl2br(Settings::contact_info())?>
-	    				</address>
-	    				<div class="social-networks">
-	    					<h2 class="title text-center">Social Networking</h2>
-							<ul>
-								<li>
-									<a href="<?=Settings::facebook_link()?>"><i class="fa fa-facebook"></i></a>
-								</li>
-								<li>
-									<a href="<?=Settings::twitter_link()?>"><i class="fa fa-twitter"></i></a>
-								</li>
-								<li>
-									<a href="<?=Settings::google_plus_link()?>"><i class="fa fa-google-plus"></i></a>
-								</li>
-								<li>
-									<a href="#"><i class="fa fa-youtube"></i></a>
-								</li>
-							</ul>
-	    				</div>
-	    			</div>
-    			</div>    			
-	    	</div>  
-    	</div>	
-    </div><!--/#contact-page-->
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-8">
+				<div class="contact-form">
+					<h2 class="title text-center">Get In Touch</h2>
 
-<?php $this->view("footer",$data); ?>
+					<?php if (is_array($errors) && count($errors) > 0) : ?>
+						<?php foreach ($errors as $error) : ?>
+							<div class="status alert alert-danger" style=""><?= $error ?></div>
+						<?php endforeach; ?>
+					<?php endif; ?>
+					<?php if (isset($_GET['success'])) : ?>
+						<div class="status alert alert-success">Message sent successfully</div>
+					<?php endif; ?>
+					<form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
+						<div class="form-group col-md-6">
+							<input type="text" name="name" class="form-control" required="required" placeholder="Name" value="<?= isset($POST['name']) ? $POST['name'] : '' ?>">
+						</div>
+						<div class="form-group col-md-6">
+							<input type="email" name="email" class="form-control" required="required" placeholder="Email" value="<?= isset($POST['email']) ? $POST['email'] : '' ?>">
+						</div>
+						<div class="form-group col-md-12">
+							<input type="text" name="subject" class="form-control" required="required" placeholder="Subject" value="<?= isset($POST['subject']) ? $POST['subject'] : '' ?>">
+						</div>
+						<div class="form-group col-md-12">
+							<textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"><?= isset($POST['subject']) ? $POST['subject'] : '' ?></textarea>
+						</div>
+						<div class="form-group col-md-12">
+							<input type="submit" class="btn btn-primary pull-right" value="Submit">
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="contact-info">
+					<h2 class="title text-center">Contact Info</h2>
+					<address>
+						<?= nl2br(Settings::contact_info()) ?>
+					</address>
+					<div class="social-networks">
+						<h2 class="title text-center">Social Networking</h2>
+						<ul>
+							<li>
+								<a href="<?= Settings::facebook_link() ?>"><i class="fa fa-facebook"></i></a>
+							</li>
+							<li>
+								<a href="<?= Settings::twitter_link() ?>"><i class="fa fa-twitter"></i></a>
+							</li>
+							<li>
+								<a href="<?= Settings::google_plus_link() ?>"><i class="fa fa-google-plus"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-youtube"></i></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div>
+	<div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=midrand+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure area map</a></iframe></div>
+	</div>
+</div><!--/#contact-page-->
+
+<?php $this->view("footer", $data); ?>
